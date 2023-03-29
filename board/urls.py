696b74f3ba317a4b7a2a1b4b324ddfe7953dbe65
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-03-29 21:23:45 +0300

    Created detail view and added task detail page

--- board/urls.py
+++ board/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from board.views import IndexListView
+from board.views import IndexListView, TaskDetailView
 
 app_name = "board"
 
 urlpatterns = [
     path("", IndexListView.as_view(), name="index"),
+    path("tasks/<int:pk>/detail", TaskDetailView.as_view(), name="task-detail")
 ]
--- board/views.py
+++ board/views.py
@@ -7,3 +7,13 @@ from board.models import Task
 class IndexListView(generic.ListView):
     model = Task
     template_name = "board/index.html"
+
+
+class TaskDetailView(generic.DetailView):
+    model = Task
+    queryset = Task.objects.prefetch_related("assignees")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    
