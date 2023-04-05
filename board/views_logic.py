963ca0cfa3c37bb59149fec1b8e6bb709218a8c5
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-06 01:25:23 +0300

    Created FilterFormMixin

--- board/views_logic.py
+++ board/views_logic.py
@@ -1,4 +1,4 @@
-from board.forms import SearchForm
+from board.forms import SearchForm, FilterForm
 
 
 class SearchFormMixin:
@@ -7,5 +7,19 @@ class SearchFormMixin:
         search_criteria = self.request.GET.get("search_criteria", "")
         context["search_form"] = SearchForm(
             initial={"search_criteria": search_criteria}
+        )
+        return context
+
+
+class FilterFormMixin:
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        filters = self.request.GET.getlist("status")
+        sorting = self.request.GET.get("deadline", "deadline")
+        context["filter_form"] = FilterForm(
+            initial={
+                "status": filters,
+                "deadline": sorting
+            }
         )
         return context
