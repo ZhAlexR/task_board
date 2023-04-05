7daf5d6a0b15d20cb1e7bc81e9b345d6800cccc2
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-06 02:27:49 +0300

    Added "get_queryset" to Mixins

--- board/views_logic.py
+++ board/views_logic.py
@@ -10,6 +10,17 @@ class SearchFormMixin:
         )
         return context
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        form = SearchForm(self.request.GET)
+
+        if form.is_valid():
+            queryset = queryset.filter(
+                name__icontains=form.cleaned_data["search_criteria"]
+            )
+
+        return queryset
+
 
 class FilterFormMixin:
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -23,3 +34,15 @@ class FilterFormMixin:
             }
         )
         return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        filter_form = FilterForm(self.request.GET)
+        if filter_form.is_valid():
+            statuses = [int(numb) for numb in filter_form.cleaned_data.get("status")]
+            deadline = filter_form.cleaned_data.get("deadline")
+            if statuses:
+                queryset = queryset.filter(status__in=statuses)
+            queryset = queryset.order_by(deadline)
+
+        return queryset
