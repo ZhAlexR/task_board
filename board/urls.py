994547650515e616033b2bd06cd22af2ebd193e6
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-15 12:50:30 +0300

    Refactored urls

--- board/urls.py
+++ board/urls.py
@@ -22,7 +22,7 @@ app_name = "board"
 urlpatterns = [
     path("", IndexListView.as_view(), name="index"),
     path(
-        "projects/create", ProjectCreateView.as_view(), name="project-create"
+        "projects/create/", ProjectCreateView.as_view(), name="project-create"
     ),
     path(
         "projects/user/<int:pk>/list/",
@@ -30,27 +30,27 @@ urlpatterns = [
         name="user-project-list",
     ),
     path(
-        "projects/<int:pk>/update",
+        "projects/<int:pk>/update/",
         ProjectUpdateView.as_view(),
         name="project-update",
     ),
     path(
-        "projects/<int:pk>/delete",
+        "projects/<int:pk>/delete/",
         ProjectDeleteView.as_view(),
         name="project-delete",
     ),
     path(
-        "projects/<int:pk>/detail",
+        "projects/<int:pk>/detail/",
         ProjectDetailView.as_view(),
         name="project-detail",
     ),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path(
-        "tasks/<int:pk>/update", TaskUpdateView.as_view(), name="task-update"
+        "tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"
     ),
     path(
-        "tasks/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"
+        "tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"
     ),
     path(
         "tasks/user/<int:pk>/list/",
